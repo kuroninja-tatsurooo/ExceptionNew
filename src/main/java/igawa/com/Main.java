45bf77552647b5f44537fd8cb23284db3ba54fef
kuroninja-tatsurooo <igatatu4@gmail.com>
2024-06-07 23:12:35 +0900

    first commit

---
+++ src/main/java/igawa/com/Main.java
@@ -0,0 +1,33 @@
+package igawa.com;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.StandardOpenOption;
+
+public class Main {
+
+  // 課題
+  // 入出力を実際に実装して、作ったファイルに文章を追加する。
+  // その作ったファイルを削除してもう一度実行すると例外になるので、その例外をキャッチして例外処理を行う。
+  // catchした中で更にファイルに書き込もうとして例外になった場合、どうなるのかを確認してください。
+  public static void main(String[] args) throws IOException {  //検査例外 ←入出力例外
+    try {
+      Path path = Path.of("JavaCourse.txt");
+      Files.writeString(path, "Java study",StandardOpenOption.APPEND); //ここが動くとファイルができる 入出力処理は例外と隣り合わせ
+      System.out.println(Files.readString(path));
+    } catch (IOException e) {
+      e.printStackTrace(); //例外の内容をコンソール画面に表示する
+      System.out.println("例外が発生した");
+      try {
+        Path path2 = Path.of("Exception.txt");
+        Files.writeString(path2,"anything write",StandardOpenOption.APPEND);
+        System.out.println(Files.readString(path2));
+      }catch (IOException exceptione){
+        exceptione.printStackTrace();
+        System.out.println("null file");
+      }
+    }
+  }
+}
